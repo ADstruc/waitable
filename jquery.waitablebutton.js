e90c3d212af5a44919ab882b3a307b48095208ff
Andrew Shults <andrewjshults@gmail.com>
2013-03-14 15:38:03 -0400

    Refactor to introduce support for being able to access a promise

--- jquery.waitablebutton.js
+++ jquery.waitablebutton.js
@@ -14,56 +14,82 @@
  */
 
 (function( $ ) {
-  $.fn.waitableButton = function(options) {
+  var NAME = 'waitableButton';
 
-    var waitingDataKey = 'waitable-button-waiting';
+  var methods = {
+    init: function(options) {
+      var settings = $.extend({
+          onClick: function() {
+              throw 'You must define an onClick function which returns a jqXhr object';
+          },
+          baseClass: 'waitable-button',
+          doneClass: 'waitable-button-done',
+          failClass: 'waitable-button-fail',
+          waitingClass: 'waitable-button-waiting'
 
-    var settings = $.extend({
-        onClick: function() {
-            throw 'You must define an onClick function which returns a jqXhr object';
-        },
-        baseClass: 'waitable-button',
-        doneClass: 'waitable-button-done',
-        failClass: 'waitable-button-fail',
-        waitingClass: 'waitable-button-waiting'
+      }, options);
 
-    }, options);
+      return this.each(function() {
+        var $el = $(this),
+            data = $el.data(NAME);
 
-    return this.each(function() {
+            if(!data) {
+              data = {
+                inProgress: false
+              };
 
-       $(this)
-       .addClass(settings.baseClass)
-       .on('click', function(e) {
-           var $el = $(this);
-           // if we are waiting, do nothing on click
-           if (true === $el.data(waitingDataKey)) {
-               // @todo add an additional class if the user
-               //       clicks button while in waiting state
-               return;
-           }
-           // put button into waiting state
-           $el.data(waitingDataKey, true);
-           // remove previous state classes and add waiting class
-           $el.removeClass(settings.doneClass);
-           $el.removeClass(settings.failClass);
-           $el.addClass(settings.waitingClass);
-           // call the user's callback and pass through context and event
-           var promise = settings.onClick.apply(this, e);
-           // @todo check that has methods implemented by promise
-           promise
-            .done(function() {
-                $el.addClass(settings.doneClass);
-            })
-            .fail(function() {
-                $el.addClass(settings.failClass);
-            })
-            .always(function() {
-                $el.removeClass(settings.waitingClass);
-                $el.data(waitingDataKey, false);
-            });
-       });
+              $el.data(NAME, data);
+            }
 
-    });
+         $el
+         .addClass(settings.baseClass)
+         .on('click', function(e) {
+            // if we are waiting, do nothing on click
+            if (true === data.inProgress) {
+              // @todo add an additional class if the user
+              //       clicks button while in waiting state
+              return;
+            }
 
+             // put button into waiting state
+            data.inProgress = true;
+            $el.removeClass(settings.doneClass)
+               .removeClass(settings.failClass)
+               .addClass(settings.waitingClass);
+
+            // call the user's callback and pass through context and event
+            var promise = settings.onClick.apply(this, e);
+            
+            if('object' !== typeof promise || 
+               'function' !== typeof promise.done || 
+               'function' !== typeof promise.fail || 
+               'function' !== typeof promise.always) {
+              $.error('Return from onClick handler does not implement promise methods');
+            }
+            
+            promise
+              .done(function() {
+                  $el.addClass(settings.doneClass);
+              })
+              .fail(function() {
+                  $el.addClass(settings.failClass);
+              })
+              .always(function() {
+                  $el.removeClass(settings.waitingClass);
+                  data.inProgress = false;
+              });
+        });
+      });
+    }
+  };
+
+  $.fn[NAME] = function(method) {
+    if(methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if('object' === typeof method || !method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error('Method ' +  method + ' does not exist on jQuery.' + NAME);
+    }
   };
 })( jQuery );
